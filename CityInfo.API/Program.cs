9e1a253cfc934b76d54ddc2a520c4c7fc9ad525b
Furqan.Zafar <furqan_zfr@outlook.com>
2025-02-06 08:09:35 +0500

    Fix: Basic project setup.

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Register servives in IOC
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//Configure request pipelines
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("Hello World....");
+});
 
 app.Run();
